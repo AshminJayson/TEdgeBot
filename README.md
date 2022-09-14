75ccdab697c52900a5412949c6326a429334b463
Ashmin Jayson <ashminjayson10@gmail.com>
2022-09-15 01:02:59 +0530

    Received JSON response from Bot

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Tutorial Link :
 https://www.pragnakalp.com/create-telegram-bot-using-python-tutorial-with-examples/
 
 Python version : 3.10.7
-PiP version : pip 22.2.2
+PiP version : 22.2.2
 
-Local development server exposed through ngrok
+Local development server tunnel 
+ngrok version : 3.0.7 installed via choco : 1.1.0
+updated to : 3.1.0 (15/9/22)
+ 
+Bot Authentication Token : 5788222546:AAGlwyvaTp4IDfkRE7_6eZ554UirVaoET4w
--- tedge.py
+++ tedge.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import request
+from flask import Response
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return "plain text"
+    if request.method == 'POST':
+        msg = request.get_json()
+        print(msg)
+
+        return Response('ok', status = 200)
+    else:
+        return "<h2>plain text</h2>"
 
 if __name__ == '__main__':
     app.run()
