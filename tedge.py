47894b24982935f6161b9b6ee054993ef45e2805
Ashmin Jayson <ashminjayson10@gmail.com>
2022-09-15 12:01:17 +0530

    Added JSON Message Parsing

--- tedge.py
+++ tedge.py
@@ -3,13 +3,23 @@ from flask import request #For recieving ChatBot POST and GET Requests
 from flask import Response #To return Response to Bot
 import requests #For messaging passing through Telegram Bot API
 
+TOKEN = "5788222546:AAGlwyvaTp4IDfkRE7_6eZ554UirVaoET4w"
+
+def parseMessage(message) :
+    # print("Message -> ", message)
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text']
+    print("Chat ID -> ", chat_id, "\nText -> ", txt)
+
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
         msg = request.get_json()
-        print(msg)
+        parseMessage(msg)
+        # print(msg)
 
         return Response('ok', status = 200)
     else:
