f7f6b50eefad2f25140e2ce5f040cb631c7a3ad0
Ashmin Jayson <ashminjayson10@gmail.com>
2022-09-17 13:37:41 +0530

    Fixed issue with message handler

--- tedge.py
+++ tedge.py
@@ -1,36 +1,40 @@
 from flask import Flask #For defining Web Application
 from flask import request #For recieving ChatBot POST and GET Requests
 from flask import Response #To return Response to Bot
+from flask import abort
 import telebot
 import requests #For messaging passing through Telegram Bot API
 
 TOKEN = "5788222546:AAEF4VgA6wQw1IBt3Vh66gODwJG-kcCHF7I"
-URL = "https://13cd-103-160-194-195.in.ngrok.io/"
+URL = "https://b409-103-183-83-106.in.ngrok.io/"
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, threaded = False)
 
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message):
-    bot.send_message(662495469, "This is a text message which isn't getting sent how I want it to.")
-    bot.reply_to(message, "Hey there ~ ")
 
-@bot.message_handler(func = lambda msg : len(msg) > 2 )
-def parseMessage(msg):
-    print(msg)
 
+#Set up webhook
 @app.route('/', methods = ['GET', 'POST'])
-def setWebHook():
-    #TelegramBot API is used to set up a webhook to the tunneling IP of ngrok
+def index() :
+    # bot.remove_webhook()
     bot.set_webhook(url = URL)
 
-    if request.method == 'POST':
-        msg = request.get_json()
-        print(msg)
+    #Setting update state so that message handler is called
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
+    
 
-    return Response('ok', status = 200)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_start_help(msg):
+    bot.send_message(2037921516, "Reshma Saju is super dumb and mean")
+    # bot.reply_to(msg, "Hey there ~ ")
 
 
 if __name__ == '__main__':
